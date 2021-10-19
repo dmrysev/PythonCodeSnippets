2e0bc4f5e50cb47b0dd83d01bdd22950e8767e6d
Dmitri Rossev <dmitrirossev@microsoft.com>
2021-10-19 12:29:47 +0300

    Added snippets

--- find_files_and_archive.py
+++ find_files_and_archive.py
@@ -3,5 +3,7 @@
 import util.filesystem
 import util.archive
 
-files = util.filesystem.find_files_recursively('C:/Users/dmitrirossev/LinuxConfig/scripts', '*.sh')
+files = list(util.filesystem.find_files_recursively('C:/Users/dmitrirossev/LinuxConfig/scripts', '*.sh'))
+files += list(util.filesystem.find_files_recursively('C:/Users/dmitrirossev/LinuxConfig/scripts', '*.py'))
 util.archive.archive_files(files, 'output.zip')
+
---
+++ redirect_std_output.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+print("test 1")
+print("test 2")
+raise Exception("test exception")
